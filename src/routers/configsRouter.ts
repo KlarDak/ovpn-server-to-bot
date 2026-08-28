64b074ab13ffe3e1443c1edb170842d69007eebe
KlarDak <mk_daking@mail.ru>
2026-08-28 15:16:09 +0300

    dev-2.1.11
    - Добавлены эндпоинты для одновременной работы с двумя и более UUID.

--- src/routers/configsRouter.ts
+++ src/routers/configsRouter.ts
@@ -3,6 +3,7 @@ import type { NextFunction, Request, Response } from 'express';
 import { verifyUuidFormat } from '../utils/verifyUtil.js';
 import { configFiles } from '../utils/configUtil.js';
 import { kickUser } from '../services/doActionUser.js';
+import { deleteFile } from '../utils/filesUtil.js';
 
 const configsRouter = Router();
 
@@ -14,87 +15,157 @@ configsRouter.use((req: Request, res: Response, next: NextFunction) => {
     return next();
 });
 
-configsRouter.patch("/", async (req: Request, res: Response) => {
+configsRouter.post("/kick", async (req: Request, res: Response) => {
     try {
-        const { uuids, type, time, status } = req.body;
+      const uuids = req.body.uuids;
 
-        if (!Array.isArray(uuids)) {
-            return res.sendServerJson(400, "MISSING_UUIDS_REQUEST");
+      if (!Array.isArray(uuids)) {
+        return res.sendServerJson(400, "MISSING_UUIDS_REQUEST");
+      }
+
+      const validUuids = uuids.filter(
+        (uuid) => verifyUuidFormat(uuid) !== false,
+      );
+      const deleteConfigs = await configFiles.updateAll({"status": "inactive"}, validUuids);
+
+      if (deleteConfigs) {
+        for (const uuid in uuids) {
+          await kickUser(uuid);
         }
 
-        if (type === undefined && time === undefined && status === undefined) {
-            return res.sendServerJson(400, "MISSING_REQUIRED_FIELDS");
+        return res.sendServerJson(204, "UUIDS_CONFIGURATION_KICKED");
+      } else {
+        return res.sendServerJson(500, "DATABASE_RECORD_UPDATE_FAILED");
+      }
+    } catch (error) {
+      console.serverError("configRouter", error);
+      return res.sendServerJson(500, "DATABASE_RECORD_UPDATE_FAILED");
+    }
+});
+
+configsRouter.post("/ban", async (req: Request, res: Response) => {
+    try {
+      const uuids = req.body.uuids;
+
+      if (!Array.isArray(uuids)) {
+        return res.sendServerJson(400, "MISSING_UUIDS_REQUEST");
+      }
+
+      const validUuids = uuids.filter(
+        (uuid) => verifyUuidFormat(uuid) !== false,
+      );
+      const deleteConfigs = await configFiles.updateAll(
+        { status: "banned" },
+        validUuids,
+      );
+
+      if (deleteConfigs) {
+        for (const uuid in uuids) {
+          await kickUser(uuid);
         }
+
+        return res.sendServerJson(204, "UUIDS_CONFIGURATION_BANNED");
+      } else {
+        return res.sendServerJson(500, "DATABASE_RECORD_UPDATE_FAILED");
+      }
+    } catch (error) {
+      console.serverError("configRouter", error);
+      return res.sendServerJson(500, "DATABASE_RECORD_UPDATE_FAILED");
+    }
+});
+
+configsRouter.post("/pardon", async (req: Request, res: Response) => {
+  try {
+    const uuids = req.body.uuids;
+
+    if (!Array.isArray(uuids)) {
+      return res.sendServerJson(400, "MISSING_UUIDS_REQUEST");
+    }
+
+    const validUuids = uuids.filter((uuid) => verifyUuidFormat(uuid) !== false);
+    const deleteConfigs = await configFiles.updateAll(
+      { status: "inactive" },
+      validUuids,
+    );
+
+    if (deleteConfigs) {
+      return res.sendServerJson(204, "UUIDS_CONFIGURATION_PARDONNED");
+    } else {
+      return res.sendServerJson(500, "DATABASE_RECORD_UPDATE_FAILED");
+    }
+  } catch (error) {
+    console.serverError("configRouter", error);
+    return res.sendServerJson(500, "DATABASE_RECORD_UPDATE_FAILED");
+  }
+});
+
+configsRouter.patch("/update", async (req: Request, res: Response) => {
+    try {
+      const { uuids, type, time } = req.body;
+
+      if (!Array.isArray(uuids)) {
+        return res.sendServerJson(400, "MISSING_UUIDS_REQUEST");
+      }
+
+      if (type === undefined && time === undefined) {
+        return res.sendServerJson(400, "MISSING_REQUIRED_FIELDS");
+      }
 
         if (type !== undefined && (typeof type !== "string" || !["user", "unlimit", "unblocked"].includes(type))) {
             return res.sendServerJson(400, "INVALID_TYPE_OR_CONTENT");
         }
 
-        if (time !== undefined && (typeof time !== "number" || time <= 0)) {
-            return res.sendServerJson(400, "INVALID_TIME_OR_CONTENT");
-        }
+      if (time !== undefined && (typeof time !== "number" || time <= 0)) {
+        return res.sendServerJson(400, "INVALID_TIME_OR_CONTENT");
+      }
 
-        if (status !== undefined && !["active", "inactive", "banned"].includes(status)) {
-            return res.sendServerJson(400, "INVALID_STATUS_OR_CONTENT");
-        }
+      const validUuids = uuids.filter(
+        (uuid) => verifyUuidFormat(uuid) !== false,
+      );
 
-        const validUuids = uuids.filter((uuid) => verifyUuidFormat(uuid) !== false);
+      const updateConfigs = await configFiles.updateAll(
+        {
+          ...(type !== undefined ? { user_type: type } : {}),
+          ...(time !== undefined ? { time } : {}),
+        },
+        validUuids,
+      );
 
-        const updateConfigs = await configFiles.updateAll({
-        ...(type !== undefined ? { user_type: type } : {}),
-        ...(time !== undefined ? { time } : {}),
-        ...(status !== undefined ? { status } : {})
-        }, validUuids);
-
-        if (updateConfigs) {
-            // TODO: Придумать, как это обыграть
-            if (type === "banned") {
-                for (const uuid in uuids) {
-                  await kickUser(uuid);
-                }
-            }
-
-            return res.sendServerJson(201, "UUIDS_CONFIGURATION_UPDATED");
-        }
-        else {
-            return res.sendServerJson(500, "DATABASE_RECORD_UPDATE_FAILED");
-        }
-    }
-    catch (error) {
-        console.serverError("configRouter", error);
+      if (updateConfigs) {
+        return res.sendServerJson(201, "UUIDS_CONFIGURATION_UPDATED");
+      } else {
         return res.sendServerJson(500, "DATABASE_RECORD_UPDATE_FAILED");
+      }
+    } catch (error) {
+      console.serverError("configRouter", error);
+      return res.sendServerJson(500, "DATABASE_RECORD_UPDATE_FAILED");
     }
 });
 
-configsRouter.delete("/", async (req: Request, res: Response) => {
-    try {
-        const uuids = req.body.uuids;
+configsRouter.delete("/drop", async (req: Request, res: Response) => {
+  try {
+    const uuids = req.body.uuids;
 
-        if (!Array.isArray(uuids)) {
-        return res.sendServerJson(400, "MISSING_UUIDS_REQUEST");
-        }
-
-        const validUuids = uuids.filter((uuid) => verifyUuidFormat(uuid) !== false);
-        const deleteConfigs = await configFiles.deleteAll(validUuids);
-
-        if (deleteConfigs) {
-            return res.sendServerJson(204, "UUIDS_CONFIGURATION_DELETED");
-        }
-        else {
-            return res.sendServerJson(500, "DATABASE_RECORD_DELETE_FAILED");
-        }
+    if (!Array.isArray(uuids)) {
+      return res.sendServerJson(400, "MISSING_UUIDS_REQUEST");
     }
-    catch (error) {
-        console.serverError("configRouter", error);
-        return res.sendServerJson(500, "DATABASE_RECORD_UPDATE_FAILED");
+
+    const validUuids = uuids.filter((uuid) => verifyUuidFormat(uuid) !== false);
+    const deleteConfigs = await configFiles.deleteAll(validUuids);
+
+    if (deleteConfigs) {
+      for (const uuid in uuids) {
+        await deleteFile(uuid);
+      }
+
+      return res.sendServerJson(204, "UUIDS_CONFIGURATION_DELETED");
+    } else {
+      return res.sendServerJson(500, "DATABASE_RECORD_DELETE_FAILED");
     }
+  } catch (error) {
+    console.serverError("configRouter", error);
+    return res.sendServerJson(500, "DATABASE_RECORD_UPDATE_FAILED");
+  }
 });
-
-configsRouter.patch("/kick");
-configsRouter.patch("/ban");
-configsRouter.patch("/pardon");
-
-configsRouter.patch("/update");
-
 
 export default configsRouter;
